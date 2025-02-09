40bd0aac25ecadee3de85c1947504d6ff5bfc366
ZikWR <3318115986@qq.com>
2025-02-09 22:26:54 +0800

    统一全局返回实体对象

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import com.nageoffer.shortlink.admin.common.convention.result.Result;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -19,8 +20,13 @@ public class UserController {
      * 根据用户名查询用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public UserRespDTO getUserByUsername(@PathVariable("username") String username) {
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        UserRespDTO result = userService.getUserByUsername(username);
+        if(result==null){
+            return new Result<UserRespDTO>().setCode("-1").setMessage("用户查询为空");
+        }else{
+            return new Result<UserRespDTO>().setCode("0").setData(result);
+        }
 
-        return userService.getUserByUsername(username);
     }
 }
--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceimpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceimpl.java
@@ -17,6 +17,20 @@ import org.springframework.stereotype.Service;
 public class UserServiceimpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
     @Override
     public UserRespDTO getUserByUsername(String username) {
+        /**
+         * LambdaQueryWrapper<UserDO>：MyBatis-Plus 提供的 条件查询构造器，用于构建 WHERE 条件的查询语句。
+         * Wrappers.lambdaQuery(UserDO.class)：创建一个 LambdaQueryWrapper<UserDO> 实例，指定查询的实体类 UserDO，方便使用 lambda 表达式指定查询字段。
+         * .eq(UserDO::getUsername, username)：添加等值查询条件，相当于 SQL 语句：
+         * SELECT * FROM t_user WHERE username = 'xxx';
+         * 等价的 Java 代码（如果不使用 MyBatis-Plus）：
+         * QueryWrapper<UserDO> queryWrapper = new QueryWrapper<>();
+         * queryWrapper.eq("username", username);
+         *
+         * baseMapper.selectOne(queryWrapper)：
+         * 调用 MyBatis-Plus 提供的 selectOne 方法，根据查询条件查找 唯一 符合的用户数据。
+         * 如果数据库中有多条数据，会抛异常，因为 selectOne 期望只返回一条记录。
+         * 查询结果存入 userDO 变量，其类型为 UserDO，即数据库中的用户实体对象。
+         */
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
                 .eq(UserDO::getUsername, username);
         UserDO userDO = baseMapper.selectOne(queryWrapper);
